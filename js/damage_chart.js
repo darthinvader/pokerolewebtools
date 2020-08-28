2cd87b8e3356a447d260954c5bf9f24106c7cbaf
LumosWitch <sofrona.anna@gmail.com>
2020-08-28 18:46:02 +0300

    Damage type chart menu linked to damage chart and home menu links to home

--- js/damage_chart.js
+++ js/damage_chart.js
@@ -1,17 +1,42 @@
 "use strict";
 
-document.getElementById("my-button").onclick = function(){
+function damageChart(){
+    var pokemon_type = ['bug','dark','dragon','electric','fairy','fighting','fire','flying','ghost','grass','ground','ice',
+                    'normal','poison','psychic','rock','steel','water'];
+
+
     $('.container').append($('<div/>', {id : 'chart'}));
-    for(var i = 1; i < 20; i++){
-        $('#chart').append($('<div/>', {'class' : 'box'}));
+    for(var i = 1; i < 19; i++){
+        $('#chart').append($('<div/>', {id: i ,'class' : 'box'}));
     }
-    $('.box').append($('<p>Super Effective Against:</p>'),$('<div/>', {'class' : 'SuperEffective'}));
-    $('.box').append($('<p>It’s Not Very Effective against:</p>'),$('<div/>', {'class' : 'NotVeryEffective'}));
-    $('#myh').append('<link rel="stylesheet" href="css/flex.css">');
+    
+    fill_the_chart(pokemon_type);
+    
 }
 
 
-function fill_the_chart(){
+function fill_the_chart(type){
 
+    var j=0;
+    
+    $('.box').append($('<h3></h3>'));
+    $('.box').append($('<p>Super Effective Against:</p>'),$('<div/>', {'class' : 'SuperEffective'}));
+    $('.box').append($('<p>It’s Not Very Effective against:</p>'),$('<div/>', {'class' : 'NotVeryEffective'}));
+    $('.box').append($('<p>No effect against:</p>'),$('<div/>', {'class' : 'NoEffect'}));
+    for (var i=1;i<=type.length;i++){
+        $('#'+i+'>h3').append($('<span/>', {'class' : type[j++]}));
+    }
+   
+}
 
+function displayHome(){
+    document.getElementById("chart").style.visibility = "hidden"; 
+}
+
+function displayChart(){
+    document.getElementById("chart").style.visibility = "visible";
+}
+
+function displayPokedex(){
+    document.getElementById("chart").style.visibility = "visible";
 }
--- js/html_strings.js
+++ js/html_strings.js
@@ -2,15 +2,17 @@
 
 var my_navbar = '\
 <li class="nav-item active">\
-    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>\
+    <a class="nav-link" href="#home" onclick="displayHome();">Home <span class="sr-only">(current)</span></a>\
 </li>\
 <li class="nav-item">\
-    <a class="nav-link" href="damagetypechart.html">Damage Type Chart</a>\
+    <a id="chartBtn" class="nav-link" href="#damage-chart" onclick="displayChart();">Damage Type Chart</a>\
 </li>\
 <li class="nav-item">\
-    <a class="nav-link" href="./fight/encounter-builder.html">Pokedex</a>\
+    <a class="nav-link" href="#pokedex" onclick="displayPokedex();">Pokedex</a>\
 </li>';
 
 $(document).ready(function () {   
     $('.navbar-nav').append(my_navbar); 
+    damageChart();
+    document.getElementById("chart").style.visibility = "hidden";
 });
