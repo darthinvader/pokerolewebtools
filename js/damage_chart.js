6889c3625412e65df2f1f4689ffcd588050a7ee8
LumosWitch <sofrona.anna@gmail.com>
2020-08-28 16:39:34 +0300

    Home page in production

---
+++ js/damage_chart.js
@@ -0,0 +1,17 @@
+"use strict";
+
+document.getElementById("my-button").onclick = function(){
+    $('.container').append($('<div/>', {id : 'chart'}));
+    for(var i = 1; i < 20; i++){
+        $('#chart').append($('<div/>', {'class' : 'box'}));
+    }
+    $('.box').append($('<p>Super Effective Against:</p>'),$('<div/>', {'class' : 'SuperEffective'}));
+    $('.box').append($('<p>It’s Not Very Effective against:</p>'),$('<div/>', {'class' : 'NotVeryEffective'}));
+    $('#myh').append('<link rel="stylesheet" href="css/flex.css">');
+}
+
+
+function fill_the_chart(){
+
+
+}
