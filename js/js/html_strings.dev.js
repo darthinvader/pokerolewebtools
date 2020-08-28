34b353e9bd73813251007f0931a9961ae3ad10f9
LumosWitch <sofrona.anna@gmail.com>
2020-08-28 16:39:34 +0300

    Home page in production

---
+++ js/js/html_strings.dev.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var my_navbar = '\
+<li class="nav-item active">\
+    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>\
+</li>\
+<li class="nav-item">\
+    <a class="nav-link" href="damagetypechart.html">Damage Type Chart</a>\
+</li>\
+<li class="nav-item">\
+    <a class="nav-link" href="#">Pokedex</a>\
+</li>';
+$(document).ready(function () {
+  $('.navbar-nav').append(my_navbar);
+});
