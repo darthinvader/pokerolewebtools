e76929225849c90b56d436de64b5c8afa1c0c7ba
LumosWitch <sofrona.anna@gmail.com>
2020-08-29 12:11:37 +0300

    damage chart is done. Fuctionality contains display methods

---
+++ js/functionality.js
@@ -0,0 +1,11 @@
+function displayHome(){
+    document.getElementById("chart").style.visibility = "hidden"; 
+}
+
+function displayChart(){
+    document.getElementById("chart").style.visibility = "visible";
+}
+
+function displayPokedex(){
+    document.getElementById("chart").style.visibility = "visible";
+}
